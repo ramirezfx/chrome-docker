789ced2acbd5ecce9e52d54cc1eb876b2a5f2746
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-01 09:36:34 +0100

    Update startchrome.sh

--- startchrome.sh
+++ startchrome.sh
@@ -5,20 +5,21 @@ HOME="$(echo ~)"
 set -e
 
 if [[ -n "$(docker ps -qaf 'name=ramirezfx/chrome:latest')" ]]; then
-	docker restart ramirezfx/chrome:latest
+        docker restart ramirezfx/chrome:latest
 else
-	USER_UID=$(id -u)
-	USER_GID=$(id -g)
-	xhost +local:docker
+        USER_UID=$(id -u)
+        USER_GID=$(id -g)
+        xhost +local:docker
 
-	docker run --shm-size=2g --rm \
+        docker run --shm-size=2g --rm \
                 --security-opt seccomp=unconfined \
-		--env=USER_UID=$USER_UID \
-		--env=USER_GID=$USER_GID \
-		--env=DISPLAY=unix$DISPLAY \
-		-v ${HOME}/docker/chrome-home:/home/chrome \
-		--volume=/tmp/.X11-unix:/tmp/.X11-unix:ro \
-		--volume=/run/user/$USER_UID/pulse:/run/pulse:ro \
-		--name chrome \
-		ramirezfx/chrome:latest
+                --env=USER_UID=$USER_UID \
+                --env=USER_GID=$USER_GID \
+                --env=DISPLAY=unix$DISPLAY \
+                -v ${HOME}/docker/chrome-home:/home/chrome \
+                -v chrome-appdir:/Applications \
+                --volume=/tmp/.X11-unix:/tmp/.X11-unix:ro \
+                --volume=/run/user/$USER_UID/pulse:/run/pulse:ro \
+                --name chrome \
+                ramirezfx/chrome:latest
 fi
