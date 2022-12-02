4bb59b768107176cf069bfcdc9aece9111866db7
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-02 07:11:32 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,5 @@ USERID=1000
 echo "creating new $USER with UID $USERID"
 useradd -m $USER -u $USERID 
 chown -R $USER /home/$USER
-chown -R $USER /Applications/Google-Chrome.AppImage
-chgrp -R $USER /Applications/Google-Chrome.AppImage
 cd /home/$USER 
 su $USER -c "/usr/bin/google-chrome-stable"
